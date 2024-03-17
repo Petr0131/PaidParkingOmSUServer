1bd4f5d32b04cbb137ff544320a57123854fec12
“Petr <nip2001@mail.ru>
2024-03-17 17:50:23 +0600

    Initial commit

---
+++ src/main/java/com/example/PaidParkingOmSUServer/PaidParkingOmSuServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.PaidParkingOmSUServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaidParkingOmSuServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaidParkingOmSuServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/PaidParkingOmSUServer/PaidParkingOmSuServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.PaidParkingOmSUServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaidParkingOmSuServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
